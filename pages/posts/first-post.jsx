53df178a0fd7e88e6043eea3d53ad74b022f0259
alepereiraciandt <alexandrepereira@ciandt.com>
2022-08-09 04:57:01 -0300

    adding images and metadata

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,14 +1,27 @@
-import Link from "next/link"
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function firsPost() {
     return (
         <>
+        <Head>
+            <title>First Post</title>
+        </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
                 </Link>
             </h2>
+            <div>
+                <Image
+                    src="/images/profile.jpg"
+                    height={200}
+                    width={200}
+                    alt="White laces"
+                />
+            </div>
         </>
     )
 }
